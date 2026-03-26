aa9a58abdcf38ea5556abf3a952f573eb13656b1
Fernando Villarreal <fernandovillarreal@mdp.edu.ar>
2026-03-26 13:23:40 -0300

    update proteomes API address

--- assets/config.js
+++ assets/config.js
@@ -7,7 +7,7 @@
 //   window.BBC_PROTEOMES_API_BASE = 'https://your-api.example.org';
 
 // Current deployed API (temporary hostname while DuckDNS CAA is flaky):
-window.BBC_PROTEOMES_API_BASE = 'https://200.0.183.74.sslip.io';
+window.BBC_PROTEOMES_API_BASE = 'https://bbcproteomesdb.mdp.edu.ar';
 
 // Default behavior:
 // - If running on localhost: use http://localhost:8000
